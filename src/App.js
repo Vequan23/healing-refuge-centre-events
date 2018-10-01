3c502c48b2d92daa3609bab20c1c2c16cc98e9c7
Vequan23 <Vequanclark@hotmail.com>
2018-09-30 19:43:47 -0500

    implemented content toggle

--- src/App.js
+++ src/App.js
@@ -9,15 +9,28 @@ import hero2 from './imgs/hfc_stock.jpeg'
 
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      contentCopyIsVisible: false
+    };
+  }
+
+  contentCopyToggle = () => {
+    this.setState({
+      contentCopyIsVisible: !this.state.contentCopyIsVisible
+    })
+  }
   render() {
+    const { contentCopyIsVisible } = this.state
     return (
 
       <div className="wrapper">
         <nav className="primary-nav">
           <ul className="primary-nav__icon-list">
-            <li> <i class="primary-nav__icon-list-icon fab fa-facebook-square fa-2x"></i></li>
-            <li><i class="primary-nav__icon-list-icon fab fa-twitter-square fa-2x"></i></li>
-            <li><i class="primary-nav__icon-list-icon fab fa-cc-paypal fa-2x"></i></li>
+            <li> <a href="https://www.facebook.com"><i class="primary-nav__icon-list-icon fab fa-facebook-square fa-2x"></i></a></li>
+            <li><a href="https://www.twitter.com"><i class="primary-nav__icon-list-icon fab fa-twitter-square fa-2x"></i></a></li>
+            <li><a href="https://www.paypal.com"><i class="primary-nav__icon-list-icon fab fa-cc-paypal fa-2x"></i></a></li>
           </ul>
         </nav>
         <header className="header">
@@ -25,7 +38,6 @@ class App extends Component {
           <img className="header__hero" src={hero}></img>
           <p className="header__title">4 Week Therapeutic Healing Group</p>
           <h3 className="header__text">Presented By: E. LaMonica Williams, MSW , LSCW</h3>
-          {/* <button className=" header__button content-box__button">Learn More</button> */}
           <i class="header__icon content-box__icon fas fa-briefcase-medical fa-3x"></i>
 
 
@@ -36,7 +48,11 @@ class App extends Component {
             <i class="content-box__icon fas fa-cross fa-3x"></i>
             <h4 className="content-box__header">Healing through BROKENESS</h4>
             <p className="content-box__paragraph">4 Week Therapeutic Healing Group</p>
-            <button className="content-box__button">Read More</button>
+            <button onClick={this.contentCopyToggle} className="content-box__button">Read More</button>
+            <p className={contentCopyIsVisible ? "content-box__copy" : "content-box__copy--isHidden"}>I am almost certain that you’ve probably heard this statement before, “Time Heals.”  It sounds really good, especially when you’re hurting but it just isn’t the total and complete truth my dear. <br></br><br></br>  It certainly takes time to heal but it undoubtedly takes a lot more than just time to heal a broken heart or a wounded spirit. <br></br><br></br>  I want to share with you exactly what it is going to take for you to heal and not only that my friend, I want to walk alongside you in your unique healing journey in your pursuit to wholeness.</p>
+            <p className={contentCopyIsVisible ? "content-box__copy" : "content-box__copy--isHidden"}>So whatever it is that you may be dealing with and struggling through I, a Qualified Mental Health Provider (QMHP) is here to help you navigate through the pain that you’re feeling. <br></br><br></br>  No matter what it is that has gotten you in this painful broken state healing is available for you. This group has been designed especially to help facilitate you through brokenness and into your healing.<br></br><br></br>  We will cover topics surrounded around healing the heart.  We will look the unpredictable things that can happen in our lives to create a devastating heartbreak. <br></br><br></br>  We will look at and define the grief and loss process, we will identify the five stages of  grief and you’ll be able to have a much better understanding of exactly where you are in these stages. <br></br><br></br>  We will look at what it takes to heal and we will be learning and implementing evidence based practices, tools and strategies that will equip you to make measurable progress along your healing journey. <br></br><br></br>  So it any of this resonates with your spirit and your soul then this therapeutic transformative group is for you.  If you think that a friend could benefit from this group I ask you to please share it with them too.  Healing truly is the gift that keeps on giving.
+            <br></br><br></br> Bunches of Love,<br></br> <span className="context-box__signature">Lamonica</span>
+            </p>
           </section>
 
           <section className="content-box">
